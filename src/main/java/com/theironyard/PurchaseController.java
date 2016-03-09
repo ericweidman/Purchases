2bfa12be775ef3f9b53d5258387887871959437b
ericweidman <ericweidman@gmail.com>
2016-03-09 13:31:54 -0500

    Added ability to parse csv files.

--- src/main/java/com/theironyard/PurchaseController.java
+++ src/main/java/com/theironyard/PurchaseController.java
@@ -1,7 +1,52 @@
 package com.theironyard;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Created by ericweidman on 3/9/16.
  */
+@Controller
 public class PurchaseController {
+
+    @Autowired
+    PurchaseRepository purchases;
+
+    @Autowired
+    CustomerRepository customers;
+
+    @PostConstruct
+    public void parseCustomer() throws FileNotFoundException {
+        File f = new File("customers.csv");
+        Scanner scanner = new Scanner(f);
+        scanner.nextLine();
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            String column[] = line.split(",");
+            Customer customer = new Customer(column[0], column[1]);
+            customers.save(customer);
+        }
+    }
+    @PostConstruct
+    public void parsePurchase() throws FileNotFoundException {
+        File f = new File("purchases.csv");
+        Scanner scanner = new Scanner(f);
+        scanner.nextLine();
+        while (scanner.hasNext()){
+            String line = scanner.nextLine();
+            String column[] = line.split(",");
+            Purchase purchase = new Purchase(column[0], column[1], column[2], column[4]);
+            purchases.save(purchase);
+
+        }
+
+    }
+
 }
+
+
